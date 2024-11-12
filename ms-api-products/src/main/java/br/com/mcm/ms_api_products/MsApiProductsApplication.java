e7cd3afb9aa6386f8f097a6b50b1ffe62cacea2b
Marcos Cesar Morais <marcos.cesarmorais@gmail.com>
2024-11-12 11:10:43 -0300

    api de produtos criada

---
+++ ms-api-products/src/main/java/br/com/mcm/ms_api_products/MsApiProductsApplication.java
@@ -0,0 +1,13 @@
+package br.com.mcm.ms_api_products;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsApiProductsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsApiProductsApplication.class, args);
+	}
+
+}
